eeb9bd2dc3975b7d9d3001639b4b3908071f9b47
abdoxfox <46646744+abdoxfox@users.noreply.github.com>
2021-06-07 02:11:55 +0100

    Update ssh.py

--- ssh.py
+++ ssh.py
@@ -53,10 +53,10 @@ class sshRunn:
 				for line in response.stdout:
 					line = line.decode('utf-8',errors='ignore').lstrip(r'(debug1|Warning):').strip() + '\r'
 					logs(line)
-					if 'pledge: proc' in line:logs(G+'CONNECTED SUCCESSFULLY '+GR)
-					elif 'Permission denied' in line:logs(R+'Access Denied'+GR)
-					elif 'Connection closed' in line:logs(R+'Connection closed'+GR)
-					elif 'Could not request local forwarding' in line:logs(R+'Port used by another programs'+GR)
+					if 'pledge: proc' in line:self.logs(G+'CONNECTED SUCCESSFULLY '+GR)
+					elif 'Permission denied' in line:self.logs(R+'Access Denied'+GR)
+					elif 'Connection closed' in line:self.logs(R+'Connection closed'+GR)
+					elif 'Could not request local forwarding' in line:self.logs(R+'Port used by another programs'+GR)
 			
 			except KeyboardInterrupt:
 				sys.exit('stoping ..')
