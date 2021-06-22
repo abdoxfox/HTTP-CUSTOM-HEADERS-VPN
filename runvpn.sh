eee1040b4f517590a0cf322fcd8a5a4bc0de8293
abdoxfox <46646744+abdoxfox@users.noreply.github.com>
2021-06-22 18:07:14 +0100

    Update runvpn.sh

--- runvpn.sh
+++ runvpn.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 RED='\033[1;31m'
 GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
@@ -9,49 +10,87 @@ echo -e "${GREEN}Choose Connection Method:"
 echo "0 - SSH Direct "
 echo "1 - PAYLOAD "
 echo "2 - SSL "
-echo -e "3 - SSL+PAYLOAD \nEnter choice number :${SCOLOR}"
+echo -e "3 - SSL+PAYLOAD "
 
-read  mode
+read -p "Enter choice number : " mode
 find=`cat settings.ini | grep "connection_mode = " |awk '{print $3}'`
 
 sed -i "s/connection_mode = $find/connection_mode = $mode/g" settings.ini
 
 sleep 1
-screen -AmdS nohub python tunnel.py
-sleep 1
-if [ "$mode" = '0' ] || [ "$mode" = '1' ]
-then
 
-	screen -AmdS nohub python ssh.py 1
-elif [ "$mode" = '2' ] || [ "$mode" = '3' ]
-	then 
-		
-		screen -AmdS nohub python ssh.py 2
-else
-	echo -e "${RED}wrong choice\ntry again${SCOLOR}"
-	python pidkill.py
-	exit
-fi
-echo -e "${YELLOW}---logs----${SCOLOR}"
-
-sleep 10
-cat logs.txt
-
-var=`cat logs.txt | grep "CONNECTED SUCCESSFULLY"|awk '{print $4}'`
-if [ "$var" = "SUCCESSFULLY" ];then 
-	echo -e "${GREEN}---Tunneling  starts-----"
-	chmod +x proxification
-	./proxification > /dev/null
-	echo -e "${SCOLOR}"
-	iptables --flush
-	
-else
-	echo -e "${RED}failed! , check settings.ini file again ${SCOLOR}"
-fi
-
+killprocess() {
 echo -e "${RED} vpn service stopped" 
-python pidkill.py 
+python pidkill.py >> /dev/null &
 rm logs.txt
-echo -e "exiting ${SCOLOR}"
+echo -e " ${SCOLOR}"
+}
+
+function connect() {
+	screen -AmdS nohub python tunnel.py
+	sleep 1
+	if [ "$mode" = '0' ] || [ "$mode" = '1' ]
+	then
+
+		screen -AmdS nohub python ssh.py 1
+	elif [ "$mode" = '2' ] || [ "$mode" = '3' ]
+		then 
+			
+			screen -AmdS nohub python ssh.py 2
+	else
+		echo -e "${RED}wrong choice\ntry again${SCOLOR}"
+		python pidkill.py
+		exit
+	fi
+
+	echo -e "${YELLOW}---logs----${SCOLOR}"
+
+	sleep 10
+	cat logs.txt
+
+	var=`cat logs.txt | grep "CONNECTED SUCCESSFULLY"|awk '{print $4}'`
+	if [ "$var" = "SUCCESSFULLY" ];then 
+		echo -e "${GREEN}---Tunneling  starts-----"
+		chmod +x proxification
+		./proxification > /dev/null
+		echo -e "${SCOLOR}"
+		iptables --flush
+		
+	else
+		echo -e "${RED}failed! ${SCOLOR}"
+	fi
+}
+connect 
+for i in {1..3}
+do 
+	
+	killprocess
+	echo -e "${GREEN}"
+	read -p "reconnect ? [y\n] " reconnect
+	if [ "$reconnect" = 'y' ]  || [ "$reconnect" = 'Y' ]
+	then 
+		read -p "do yo want changing auto replace value [y\n]" auto_replace
+		if [ "$auto_replace" = 'y' ]  || [ "$auto_replace" = 'Y' ]
+		then
+			value=`cat settings.ini | grep "auto_replace ="| awk '{print $3}'`
+			echo "0 - for payload that doesn't send HTTP RESPONSE"
+			echo "1 - for payload that send one HTTP RESPONSE"
+			echo "2 - for payload that send double HTTP RESPONSE"
+			read -p "Enter your sutable choice : " choice 
+
+			sed -i "s/auto_replace = $value/auto_replace = $choice/g" settings.ini
+		 else
+		 	echo ''
+		 fi
+		echo -e "reconnecting ${SCOLOR}"
+
+		connect
+	else 
+		exit
+	fi 
+
+done
+
+
 
 
