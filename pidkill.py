aac9c4dd12aa2440b596d7e398560c453488650c
abdoxfox <46646744+abdoxfox@users.noreply.github.com>
2022-09-17 21:33:09 +0100

    Update pidkill.py

--- pidkill.py
+++ pidkill.py
@@ -6,7 +6,6 @@ def handler():
     output, error = cmd.communicate()
     for line in output.splitlines():
             pid = line.split(b'/')[0].decode()
-            print(pid)
             os.system(f'kill {pid}')
 
 handler()
