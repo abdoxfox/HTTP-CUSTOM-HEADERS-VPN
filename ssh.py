f4666c80bc76956e65c8db10b5de73f33cd9a758
abdoxfox <46646744+abdoxfox@users.noreply.github.com>
2022-09-17 21:30:23 +0100

    Update ssh.py
    Fix 'already port used or binded'

--- ssh.py
+++ ssh.py
@@ -96,7 +96,7 @@ class sshRunn:
 		password = config['ssh']['password']
 		self.enableCompress = config['ssh']['enable_compression']
 		self.create_connection(host,port,user,password)
-		
+localport = sys.argv[2]		
 start = sshRunn('127.0.0.1','9092')
 start.main()
 		
