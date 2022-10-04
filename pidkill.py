ef5797e9337b8e3f24927e7eefd5927b54aee2b5
abdoxfox <46646744+abdoxfox@users.noreply.github.com>
2022-10-04 10:28:47 +0100

    Add files via upload

--- pidkill.py
+++ pidkill.py
@@ -1,11 +1,17 @@
 import subprocess
-import os
+import os,sys
 def handler():
     print('killing process ...')
     cmd = subprocess.Popen(("sudo netstat -anp|grep screen|awk '{print$9}'"),shell=True, stdout=subprocess.PIPE)
     output, error = cmd.communicate()
     for line in output.splitlines():
             pid = line.split(b'/')[0].decode()
+            print(pid)
             os.system(f'kill {pid}')
-
+try:
+	if sys.argv[1] :
+		os.system('su -c am start --user 0 -n com.termux/com.termux.app.TermuxActivity && sudo pkill bash >> /dev/null')
+		os.system('sudo iptables -t nat -F OUTPUT')
+		os.system('sudo rm logs.txt')
+except:pass
 handler()
