710a4e7c0c04612dc155bc29c2bf02ed5a0fe2f1
abdoxfox <46646744+abdoxfox@users.noreply.github.com>
2023-04-10 05:10:49 +0000

    Add files via upload

--- inject.py
+++ inject.py
@@ -2,6 +2,7 @@ import time
 import socket 
 import configparser
 import re
+import sys
 
 
 bg=''
@@ -43,7 +44,7 @@ class injector():
 
 
 	def payloadformating(self,payload,host,port):
-		self.logs(f'{O}[TCP] Sending payload :\n{payload}{GR}')
+		
 		payload = payload.replace('[crlf]','\r\n')
 		payload = payload.replace('[crlf*2]','\r\n\r\n')
 		payload = payload.replace('[cr]','\r')
@@ -64,83 +65,40 @@ class injector():
 		payload = payload.replace('[host]',host)
 		payload = payload.replace('[port]',port)
 		payload = payload.replace('[auth]','')
+		payload = payload.replace('[split]' ,'=1.0=')
+		payload = payload.replace('[delay_split]'  ,'=1.5=')
+		payload = payload.replace('[instant_split]','=0.0=')
 		return payload
 
-	def connection(self,client, s,host,port):
+	def connection(self,client, server,host,port):
 	        if int(self.conn_mode(self.conf())) == 0:
-	        	payload = b'SSH-2.0-abdoxfox'
+	        	return self.get_resp(server=server,client=client)
 	        			       
 	        else:
-	        	payload = self.payloadformating(self.getpayload(self.conf()),host,port)
-	        
-	        if '[split]' in payload or '[instant_split]' in payload or '[delay_split]' in payload:
-	          payload = payload.replace('[split]'        ,'||1.0||')
-	          payload = payload.replace('[delay_split]'  ,'||1.5||')
-	          payload = payload.replace('[instant_split]','||0.0||')
-	          
-	          req = payload.split('||')
-	          
-	          for payl in req:
-	              if payl in ['1.0','1.5','0.0'] :
-	                delay = payl
-	                time.sleep(float(delay))
+	        	payloads = self.payloadformating(self.getpayload(self.conf()),host,port).split('=')
+	        	
+	        for payload in payloads:
+	              if payload in ['1.0','1.5','0.0'] :
+	                time.sleep(float(payload))
 	              else:
-	                s.send(payl.encode())
-	        
-	        elif '[repeat_split]' in payload  :
-	          payload = payload.replace('[repeat_split]','||1||')
-	          payload = payload.replace('[x-split]','||1||')
-	          req = payload.split('||')
-	          payl = []
-	          for element in req:
-	            if element and element == '1' :pass
-	            else:payl.append(element)
-	          rpspli = payl[0]+payl[0]
-	          s.send(rpspli.encode())
-	          s.send(payl[1].encode())
+	                self.logs(f'{O} sending payload : {payload.encode()}{GR}')
+	                server.send(payload.encode())
+	        return self.get_resp(server=server,client=client)
 
-	        elif '[reverse_split]' in payload or '[x-split]' in payload:
-	          payload = payload.replace('[reverse_split]','||2|')
-	          payload = payload.replace('[x-split]','||2|')
-	          req = payload.split('||')
-	          payl = []
-	          for element in req:
-	            if element and element == '2':pass
-	            else:payl.append(element)
-	          rvsplit = payl[0]+payl[1]
-	          s.send(rvsplit.encode())
-	          s.send(payl[1].encode())
-
-	        elif '[split-x]' in payload:
-	          payload = payload.replace('[split-x]','||3||')
-	          req = payload.split('||')
-	          xsplit = []
-	          for element in req:
-	            if element and element == '3':pass
-	            else:xsplit.append(element)
-	          alpay = xsplit[0]+xsplit[1]
-	          s.send(alpay.encode())
-	          
-	          time.sleep(1.0)
-	          s.send(xsplit[1].encode())
-	        else:
-	          print(payload)
-	          s.send(payload.encode())
-	        self.get_resp(s,client)
 	def get_resp(self,server,client) :
 		packet = server.recv(1024)
 		res = packet.decode('utf-8','ignore')
 		status = res.split('\n')[0]
 		if status.split('-')[0]=='SSH':
-			self.logs(f'{O}response : {G}{status}{GR}')
+			self.logs(f'response : {status}')
 			client.send(packet)
 			return True
 		else:
-			if status:
-                            if re.match(r'HTTP/\d(\.\d)? \d\d\d ',status):
-                                self.logs(f'{O}response : {G}{status}{GR}')
-                                client.send(b'HTTP/1.1 200 Connection established\r\n\r\n')
-                                return self.get_resp(server,client)		
+			if re.match(r'HTTP/\d(\.\d)? \d\d\d ',status):
+				self.logs(f'response : {status}')
+				client.send(b'HTTP/1.1 200 Connection established\r\n\r\n')
+				return self.get_resp(server,client)
+
 	def logs(self,log):
 		logtime = str(time.ctime()).split()[3]
 		logfile = open('logs.txt','a')
