bf571166161228cbf519ef4d0dd71bc8aaa709c1
abdoxfox <46646744+abdoxfox@users.noreply.github.com>
2023-04-25 10:25:19 +0000

    Update runvpn.sh

--- runvpn.sh
+++ runvpn.sh
@@ -9,7 +9,7 @@ RED='\033[1;31m'
 GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
 SCOLOR='\033[0m'
-if [ -d "redsocks/" ]   
+if [ -f "redsocks/" ]   
 then 
 echo "${GREEN}Redsocks installed ${SCOLOR}"
 else
