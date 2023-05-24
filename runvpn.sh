62db2613f2d2099eb070993e2716252658f467c8
abdoxfox <46646744+abdoxfox@users.noreply.github.com>
2023-05-24 16:58:49 +0100

    Add files via upload

--- runvpn.sh
+++ runvpn.sh
@@ -9,7 +9,7 @@ RED='\033[1;31m'
 GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
 SCOLOR='\033[0m'
-if [ -f "redsocksv" ]   
+if [ -f "redsocksv" ] 
 then 
 echo "${GREEN}Redsocks installed ${SCOLOR}"
 else
@@ -19,40 +19,25 @@ unzip redsocks
 cd redsocks 
 make
 cp redsocks redsocksv
-cp redsocksv ..
+cp redsocksv ../
+cd ..
+unzip dns2socks.zip
+cd dns2socks
+make
+chmod 777 dns2socks
+cp dns2socks "$PREFIX"/bin
+sleep 10
 fi
 EOF
 bash redsocksSetup.sh
 rm redsocksSetup.sh
 rm -rf redsocks
+rm -rf dns2socks
 clear
-function modeChange() {
-echo -e "${GREEN}Choose Connection Method:"
-echo "0 - SSH Direct "
-echo "1 - PAYLOAD "
-echo "2 - SSL "
-echo -e "3 - SSL+PAYLOAD "
 
-read -p "Enter choice number : " mode
 
-con_mode=`cat settings.ini | grep "connection_mode = " |awk '{print $3}'`
-
-sed -i "s/connection_mode = $con_mode/connection_mode = $mode/g" settings.ini
-
-}
-
-dnsenable="y"
-if [ "$dnsenable" = "y" ] || [ "$dnsenable" = "Y" ] 
-
-  then
-     sed -i "s/#iptables -t nat -A PROXY -p udp/iptables -t nat -A PROXY -p udp/g" proxification
-
-else
-   if [ $(cat proxification |grep "nat -A PROXY -p udp" | awk '{print $1;exit}') != "#iptables" ]
-    then
-     sed -i "s/iptables -t nat -A PROXY -p udp/#iptables -t nat -A PROXY -p udp/g" proxification
-    fi
-fi
+m=`cat settings.ini |sed -e '1p' -e '/connection_mode/!d'| awk '{print $3}'`
+mode="$m"
 
 killprocess() {
 echo -e "${RED} KILLING PROCESS...." 
@@ -60,23 +45,23 @@ python3 pidkill.py >>/dev/null
 rm -rf logs.txt
 echo -e " DONE ${SCOLOR}"
 }
-modeChange
+
 function connect() {
         localport="$1"
-	
-	if [ "$mode" = '0' ] 
+
+	if [ $mode = 0 ] 
         then
            screen -AmdS nohup python3 ssh.py 0 _
-        elif [ "$mode" = '1' ]
+        elif [ $mode = "1" ]
 	  then
                screen -AmdS nohup python3 tunnel.py $localport
                sleep 1
-               screen -AmdS nohub python3 ssh.py 1 $localport
+               screen -AmdS nohub sudo python3 ssh.py 1 $localport
 	elif [ "$mode" = '2' ] || [ "$mode" = '3' ]
 		then 
 			screen -AmdS nohup python3 tunnel.py $localport
                         sleep 1
-			screen -AmdS pythonwe python3 ssh.py 2  $localport 
+			screen -AmdS pythonwe python3 ssh.py 1  $localport 
 	else
 		echo -e "${RED}wrong choice\ntry again${SCOLOR}"
 		killprocess
@@ -93,7 +78,8 @@ function connect() {
 	if [ "$var" = "SUCCESSFULLY" ];then 
 		echo -e "${GREEN}---Tunneling  starts-----${SCOLOR}"
 		chmod +x proxification
-		sudo bash proxification >> /dev/null 
+		sudo ./proxification >> /dev/null 
+	
                
 		sudo iptables -t nat -F OUTPUT
 		
@@ -108,7 +94,4 @@ do
 	killprocess
 	connect $i 
 done
-
-
-
 
