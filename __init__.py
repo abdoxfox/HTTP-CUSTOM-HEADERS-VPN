c68808038050cde2063b2d287edfd1b2034ef12e
abdoxfox <46646744+abdoxfox@users.noreply.github.com>
2024-04-13 13:55:13 +0000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,14 +1,5 @@
 from src.tunnel import Tun
 class injector_init_():
     def __init__(self):
-        self.Tun = Tun()
-    def main(self):
-        self.Tun.create_connection()
-        
-        
-     
-if __name__=="__main__":
-    run= injector_init_()
-    run.main()
-       
+        pass
        
