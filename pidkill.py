36093e81d130ab4f2915c64a8d6c13bf2ce755e5
abdoxfox <46646744+abdoxfox@users.noreply.github.com>
2024-04-13 13:50:04 +0000

    Add files via upload

---
+++ pidkill.py
@@ -0,0 +1,11 @@
+import subprocess
+import os
+
+def handler(thread):
+    cmd = subprocess.Popen(("netstat -anp|grep "+thread+"|awk '{print$7}'"),shell=True, stdout=subprocess.PIPE)
+    output, error = cmd.communicate()
+    for line in output.splitlines():
+            pid = line.split(b'/')[0].decode()
+            os.system(f'kill {pid}')
+
+
